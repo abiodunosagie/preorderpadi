0461a9a2978180238dfe382d6cf8395d0ef11db2
Abiodun Osagie <abiodunosagie8@gmail.com>
2023-11-27 11:08:50 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//main function
+void main() {
+  runApp(const Preorderpadi());
+}
+
+class Preorderpadi extends StatelessWidget {
+  const Preorderpadi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
