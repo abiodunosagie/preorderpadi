f07a8e2f31a2bff45274973a1c9e120dfafdc604
Abiodun Osagie <abiodunosagie8@gmail.com>
2023-12-08 10:59:28 +0100

    horizontal scrollable images categories section

--- lib/common/widgets/custom_shapes/containers/primary_header_container.dart
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -17,7 +17,7 @@ class TPrimaryHeaderContainer extends StatelessWidget {
         color: TColors.primary,
         padding: const EdgeInsets.only(bottom: 0),
         child: SizedBox(
-          height: 400,
+          height: 350,
           child: Stack(
             children: [
               Positioned(
